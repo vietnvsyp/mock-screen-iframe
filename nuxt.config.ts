ca1d820db811a6bb662c3d29580cc4ff567fbb09
SYP <dominhkhoi@syp.vn>
2023-06-23 19:40:38 +0700

    mock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,6 +52,6 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    baseURL: '/top',
+    baseURL: '/',
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,47 @@
 <template>
-  <div class="d-flex iframe-item">
+  <!-- <div class="d-flex iframe-item">
     <div class="d-flex justify-center align-center sidebar gray-background">
       <span>コンテンツ</span>
     </div>
-    <div class="main">
-      <div class="header gray-background">
-        <form @submit="onSubmit" class="form-access">
-          <TextField
-            label="Access token"
-            name="access_token"
-            v-model="accessToken"
-          ></TextField>
-          <v-btn type="submit" block class="mt-2">Submit</v-btn>
-        </form>
-      </div>
+    <div class="header gray-background">
+      <form @submit="onSubmit" class="form-access">
+        <TextField
+          label="Access token"
+          name="access_token"
+          v-model="accessToken"
+        ></TextField>
+        <v-btn type="submit" block class="mt-2">Submit</v-btn>
+      </form>
+    </div> -->
+  <div class="sidenav"><img src="/left-menu.png" /></div>
+  <div
+    style="
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      min-height: 100%;
+      font-family: 'Yu Gothic';
+    "
+    class="gray-background"
+  >
+    <div class="header">
+      <form @submit="onSubmit" class="form-access">
+        <img src="/top-menu.png" style="height: 150px" />
+      </form>
+    </div>
+    <div class="main gray-background">
       <iframe
         id="webB"
-        width="100%"
-        height="2000px"
-        class=""
-        src="https://syp-dev.d1ki13c332hn49.amplifyapp.com/"
+        class="responsive-iframe"
+        src="https://syp-dev.dydulkk5p8dko.amplifyapp.com/"
         title="Create Member"
         scrolling="no"
+        onload="this.style.height=(this.contentWindow.document.body.scrollHeight+20)+'px';"
       >
       </iframe>
     </div>
   </div>
+  <!-- </div> -->
 </template>
 
 <script setup lang="ts">
@@ -33,7 +49,7 @@ import { useForm } from 'vee-validate'
 import { ref } from 'vue'
 import { object, string } from 'yup'
 
-const accessToken = ref('')
+const accessToken = ref('aaaaaa')
 const refreshToken = ref('')
 
 onMounted(() => {
@@ -95,22 +111,50 @@ function onInvalidSubmit({
   console.log(errors) // a map of field names and their first error message
   console.log(results) // a detailed map of field names and their validation results
 }
+function resizeIframe(obj: any) {
+  obj.style.height =
+    obj.contentWindow.document.documentElement.scrollHeight + 'px'
+}
 </script>
 
 <style lang="scss" scoped>
 .gray-background {
-  background: $gray-background;
+  //background: $gray-background;
+  background: white;
+}
+.header {
+  width: 100%;
+  background: white;
+  margin-left: auto;
+  margin-right: auto;
 }
 .form-access {
-  width: 50%;
+  width: 40%;
   margin: 0 auto;
   padding-bottom: 16px;
-}
-.sidebar {
-  width: 13%;
-  height: 742px;
+  height: 150px;
 }
 .main {
-  width: 87%;
+  margin-left: 180px;
+  flex: 1;
+  width: 80%;
+}
+.responsive-iframe {
+  top: 0;
+  left: 10%;
+  bottom: 0;
+  right: 10%;
+  width: 100%;
+  height: 4500px;
+  //border: solid 1px black;
+  border: none;
+}
+.sidenav {
+  height: 100%;
+  width: 180px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
 }
 </style>
